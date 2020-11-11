093cd8844ac84723241d441ac5cac5a5aead31c0
Oumaima-debbabi <58271521+Oumaima-debbabi@users.noreply.github.com>
2020-11-11 18:58:59 +0100

    Update Purchase.java

--- src/purchase/Purchase.java
+++ src/purchase/Purchase.java
@@ -18,27 +18,35 @@ public class Purchase {
         Scanner scanner = new Scanner( System.in );
      
         Produit s=new Produit();
-   
-     
+        boolean res=false;
+         
          try{
+              
              //entrer la valeur de quantité
                System.out.println("Please enter the quantity");
                //lire la valeur entrée
               int q = scanner.nextInt();
+ 
                //entrer le prix 
+              
                System.out.println("Please enter price"); 
                //lire le prix
                float p=scanner.nextFloat();
                //calculer et afficher le prix totale 
+               if(q>0 &&p>00){
                System.out.println( "the total price for the given quantity= "+(s.calculateTotalPrice(q,p)));
+              
+               } else{
+                   System.out.println("Wrong try again");
+               }
            }
             //une fois l'une de valeur entrée est invalide une expcetion est detectée
          catch(InputMismatchException e){
                      System.out.println("Wrong");
          }
-      
     }
-}
+    }
+    
 
                    
           
